3255c42574cf2fb18dbe5e14b6da46796be6188e
yaya-nyan <Florian.Denker@live.de>
2021-01-11 18:34:51 +0100

    added the showTitle Event

--- Assets/Scripts/EventSystem.cs
+++ Assets/Scripts/EventSystem.cs
@@ -8,6 +8,13 @@ public class EventSystem
     public delegate void intAction(int i);
     public static event intAction onHealthUpdate;
     public static event Action onPlayerDeath;
+    public delegate void titleDelegate(string text, float charDelay, float showTime);
+    public static event titleDelegate onTitle;
+
+    public static void ShowTitle(string text, float charDelay, float showTime)
+    {
+        onTitle?.Invoke(text, charDelay, showTime);
+    }
 
     public static void HealthUpdate(int amount)
     {
--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HUD : MonoBehaviour
 {
@@ -10,13 +11,20 @@ public class HUD : MonoBehaviour
     public Sprite DeathPopup;
     public SpriteRenderer BlackoutSquare;
     public Camera GameOverCamera;
+    public TextMeshProUGUI Title;
 
     Coroutine damagePopUpCoroutine;
+    Coroutine TitleCoroutine;
 
     private void Start()
     {
         EventSystem.onHealthUpdate += HealthUpdate;
         EventSystem.onPlayerDeath += PlayerDeath;
+        EventSystem.onTitle += (string text, float charDelay, float displayTime) => 
+        {
+            if (TitleCoroutine != null) StopCoroutine(TitleCoroutine);
+            TitleCoroutine = StartCoroutine(TypeTitle(text, charDelay, displayTime));
+        };
     }
 
     void HealthUpdate(int health)
@@ -77,6 +85,22 @@ public class HUD : MonoBehaviour
             time += Time.deltaTime * 2;
             BlackoutSquare.color = new Color(0, 0, 0, Mathf.Floor(time * 8) / 8);
         }
+    }
+
+    IEnumerator TypeTitle(string text, float charDelay, float displayTime)
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            Title.text += text[i];
+            yield return new WaitForSeconds(charDelay);
+        }
+        yield return new WaitForSeconds(displayTime);
+        for (int i = 0; i < text.Length; i++)
+        {
+            Title.text = Title.text.Substring(0, Title.text.Length - 1);
+            yield return new WaitForSeconds(charDelay / 2);
+        }
+        TitleCoroutine = null;
     }
 
     private void OnDestroy()
